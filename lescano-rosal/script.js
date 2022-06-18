7cbc7e0b10429c9e824822f76f1934ccf9337af2
Enzojbl <102130388+Enzojbl@users.noreply.github.com>
2022-06-19 02:26:11 +0800

    Update script.js

--- lescano-rosal/script.js
+++ lescano-rosal/script.js
@@ -1,1 +1,9 @@
-
+window.onload = function () {
+    AFRAME.registerComponent("void-event", {
+    init: function () {
+      this.audio = document.querySelector("#matrix")
+      let void = document.querySelector("#void-model")
+      void = void.setAttribute("animation-mixer", "clip: Static Pose")
+      this.el.addEventListener(
+        "click",
+        
