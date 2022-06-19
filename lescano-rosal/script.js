63f19e12024360d6f518ff9fbb676642454ddaa0
Enzojbl <102130388+Enzojbl@users.noreply.github.com>
2022-06-19 15:43:55 +0800

    Update script.js

--- lescano-rosal/script.js
+++ lescano-rosal/script.js
@@ -5,5 +5,58 @@ window.onload = function () {
       let void = document.querySelector("#void-model")
       void = void.setAttribute("animation-mixer", "clip: Static Pose")
       this.el.addEventListener(
-        "click",
-        
+        "click", 
+        function (e) {
+            let void = document.querySelector("#void-model")         
+            console.log(void.getAttribute("animation-mixer").clip)
+            if(void.getAttribute("animation-mixer").clip === "Animation"){
+                vois.setAttribute("animation-mixer", "clip: Static Pose")   
+                this.audio.pause()
+                this.audio.currentTime = 0
+            }else{
+                void.setAttribute(
+                    "animation-mixer",
+                    "clip: Animation; loop: repeat; timeScale: 3.5"
+                    )
+                    this.audio.play()
+                    this.audio.loop = true
+                } 
+          console.log(e)
+        }.bind(this)
+      )
+               this.el.addEventListener('mouseenter', function (e) {
+                    console.log(e)
+                }.bind(this));
+		    
+		 this.el.addEventListener('mouseleave', function (e) {
+			 console.log(e)
+                }.bind(this));
+		 
+		
+                this.el.addEventListener('touchmove', function (e) {
+                    //this.audio.play();
+			
+                  console.log(e)
+                }.bind(this));
+                this.el.addEventListener('touchstart', function (e) {
+			let void = document.querySelector('#void-model')
+		    void.setAttribute('animation-mixer', "clip: Animation; loop: repeat; timeScale: 2");
+                    this.audio.play();
+			
+			console.log(e)
+                }.bind(this));
+		    
+		 this.el.addEventListener('touchend', function (e) {
+			 console.log(e)
+                }.bind(this));
+		    
+		
+                this.el.addEventListener('animation-finished', function (e) {
+			 console.log(e)
+                    let void = document.querySelector('#void-model')
+		    void.removeAttribute('animation-mixer');
+                }.bind(this));
+		
+            }
+        });
+    };
