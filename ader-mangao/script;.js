797608d8e1ea38140574abde14bbe2024693ff9a
MangaoKheandramarion <102133202+MangaoKheandramarion@users.noreply.github.com>
2022-07-12 01:33:57 +0800

    Create script;.js

---
+++ ader-mangao/script;.js
@@ -0,0 +1,89 @@
+      //JohannesVeremeer COMPONENTS//
+      AFRAME.registerComponent("GirlWithAPearlEarring-event", {
+        init: function () {
+          console.log("init");
+          let JohannesVermeerBibliography = $("#JohannesVermeerBibliography ");
+          this.el.addEventListener(
+            "click",
+            function (e) {
+            if (JohannesVermeerBibliography .attr('visible') === false) {
+              JohannesVermeerBibliography attr("visible", true);
+            } else {
+              JohannesVermeerBibliography .attr("visible", false);
+            }
+              console.log(JohannesVermeerBibliography attr('visible'));
+            }.bind(this)
+            )
+            this.el.addEventListener(
+                "touch",
+                function (e) {
+                if (JohannesVermeerBibliography .attr('visible') === false) {
+                  JohannesVermeerBibliography .attr("visible", true);
+                } else {
+               JohannesVermeerBibliography .attr("visible", false);
+                }
+                  console.log(JohannesVermeerBibliography .attr('visible'));
+                }.bind(this)
+              )
+        },
+      });
+      
+      //LeonardodaVinci COMPONENTS//
+       AFRAME.registerComponent("MonaLisa-event", {
+        init: function () {
+          console.log("init");
+          let LeonardodaVinciBibliography = $("#LeonardodaVinciBibliography");
+          this.el.addEventListener(
+            "click",
+            function (e) {
+            if (LeonardodaVinciBibliography.attr('visible') === false) {
+            LeonardodaVinciBibliography.attr("visible", true);
+            } else {
+             LeonardodaVinciBibliography.attr("visible", false);
+            }
+              console.log(LeonardodaVinciBibliography.attr('visible'));
+            }.bind(this)
+          )
+          this.el.addEventListener(
+            "touch",
+            function (e) {
+            if (LeonardodaVinciBibliography.attr('visible') === false) {
+              LeonardodaVinciBibliography.attr("visible", true);
+            } else {
+             LeonardodaVinciBibliography.attr("visible", false);
+            }
+              console.log(LeonardodaVinciBibliography.attr('visible'));
+            }.bind(this)
+          )
+        },
+      });
+
+            //VincentVanGogh COMPONENTS//
+        AFRAME.registerComponent("TheStarryNight-event", {
+        init: function () {
+          console.log("init");
+          let VincentVanGoghBibliography = $("#VincentVanGoghBibliography");
+          this.el.addEventListener(
+            "click",
+            function (e) {
+            if (VincentVanGoghBibliography.attr('visible') === false) {
+              VincentVanGoghBibliography.attr("visible", true);
+            } else {
+              VincentVanGoghBibliography.attr("visible", false);
+            }
+              console.log(VincentVanGoghBibliography.attr('visible'));
+            }.bind(this)
+          )
+          this.el.addEventListener(
+            "touch",
+            function (e) {
+            if (VincentVanGoghBibliography.attr('visible') === false) {
+              VincentVanGoghBibliography.attr("visible", true);
+            } else {
+              VincentVanGoghBibliography.attr("visible", false);
+            }
+              console.log(VincentVanGoghBibliography.attr('visible'));
+            }.bind(this)
+          )
+        },
+      });
